be46c479cbc1646ef9b21e4a8d0b7b49eac1a931
Bishop <bishopuzochukwu37@gmail.com>
2022-10-06 19:43:28 +0100

    added google sign in

--- lib/src/features/authentication/data/auth_repository.dart
+++ lib/src/features/authentication/data/auth_repository.dart
@@ -1,12 +1,52 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
 abstract class AuthRepository{
-  Future<void> signInWithGoogle();
+  Future<User?> signInWithGoogle();
+  Future<void> signOut();
+  Stream<User?> authStateChanges();
 }
 
 class FirebaseAuthRepo extends AuthRepository {
+   final _firebaseAuth = FirebaseAuth.instance;
+
+  
   @override
-  Future<void> signInWithGoogle() {
-    // TODO: implement signInWithGoogle
-    throw UnimplementedError();
+  Future<void> signOut() async {
+    await _firebaseAuth.signOut();
+    final googleSignIn = GoogleSignIn();
+
+    await googleSignIn.signOut();
+    
+  }
+
+
+  @override
+  Stream<User?> authStateChanges() => _firebaseAuth.userChanges();
+
+  @override
+  Future<User?> signInWithGoogle() async {
+    final googleSignIn = GoogleSignIn();
+    final googleUser = await googleSignIn.signIn();
+    if (googleUser != null) {
+      final googleAuth = await googleUser.authentication;
+      if (googleAuth.idToken != null) {
+        final userCredential = await _firebaseAuth
+            .signInWithCredential(GoogleAuthProvider.credential(
+          idToken: googleAuth.idToken,
+          accessToken: googleAuth.accessToken,
+        ));
+
+        return userCredential.user;
+      } else {
+        throw FirebaseAuthException(
+            code: 'ERROR_MISSING_GOOGLE_IDTOKEN',
+            message: 'Missing Google IDtoken');
+      }
+    } else {
+      throw FirebaseAuthException(
+          code: 'ERROR_ABORTED_BY_USER');
+    }
   }
 
 }
