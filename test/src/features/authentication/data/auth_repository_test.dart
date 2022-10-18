7d025afecf375b859ef0841c38252abf3ec7d913
Bishop <bishopuzochukwu37@gmail.com>
2022-10-18 19:53:28 +0100

    fixed auth repo unit tests

--- test/src/features/authentication/data/auth_repository_test.dart
+++ test/src/features/authentication/data/auth_repository_test.dart
@@ -6,22 +6,19 @@ import '../../../mocks.dart';
 
 void main() {
   group('Auth repository tests', () {
-    late final MockAuthRepository authRepository;
-    late final MockUser mockUser;
-
-    setUp(() {
-      authRepository = MockAuthRepository();
-      mockUser = MockUser();
-    });
 
     test('current user is null before sign in', () {
+      final authRepository = MockAuthRepository();
+    
       when(authRepository.authStateChanges)
           .thenAnswer((_) => Stream.value(null));
       expect(authRepository.currentUser, null);
       expect(authRepository.authStateChanges(), emits(null));
     });
-    
+
     test('current user is not null when sign in succeeds', () async {
+       final authRepository = MockAuthRepository();
+      final mockUser = MockUser();
       when(authRepository.signInWithGoogle).thenAnswer((_) => Future.value());
       when(authRepository.authStateChanges)
           .thenAnswer((_) => Stream.value(mockUser));
@@ -32,19 +29,24 @@ void main() {
     });
 
     test('current user is null after sign out', () async {
+       final authRepository = MockAuthRepository();
       when(authRepository.signOut).thenAnswer((_) => Future.value());
       when(authRepository.authStateChanges)
           .thenAnswer((_) => Stream.value(null));
+      when(() => authRepository.currentUser).thenReturn(null);
       await authRepository.signOut();
       expect(authRepository.currentUser, null);
       expect(authRepository.authStateChanges(), emits(null));
     });
 
     test('sign in throws when missing google id', () async {
+       final authRepository = MockAuthRepository();
+     
       const exception = AppException.invalidGoogleIdToken();
       when(authRepository.signInWithGoogle).thenThrow(exception);
       when(authRepository.authStateChanges)
           .thenAnswer((_) => Stream.value(null));
+      when(() => authRepository.currentUser).thenReturn(null);
       await expectLater(authRepository.signInWithGoogle,
           throwsA(const AppException.invalidGoogleIdToken()));
       expect(authRepository.currentUser, null);
@@ -52,10 +54,13 @@ void main() {
     });
 
     test('sign in throws when sign in canceled', () async {
+       final authRepository = MockAuthRepository();
+     
       const exception = AppException.signInCanceled();
       when(authRepository.signInWithGoogle).thenThrow(exception);
       when(authRepository.authStateChanges)
           .thenAnswer((_) => Stream.value(null));
+      when(() => authRepository.currentUser).thenReturn(null);
       await expectLater(authRepository.signInWithGoogle,
           throwsA(const AppException.signInCanceled()));
       expect(authRepository.currentUser, null);
